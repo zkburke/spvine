19727944371919ad840a9c1681481760c9a0d144
thedoctorquantum <drofquantum@protonmail.com>
2023-03-15 23:21:54 +0000

    Tokenizer handles multi and single line comments and keywords

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const Tokenizer = @This();
 
 source: []const u8,
@@ -7,9 +8,14 @@ pub fn next(self: *Tokenizer) ?Token {
     var state: enum {
         start,
         identifier,
+        slash,
+        single_comment,
+        multi_comment,
     } = .start;
     
     var token = Token { .start = self.index, .end = self.index, .tag = .end };
+
+    var multi_comment_level: usize = 0;
 
     while (self.index < self.source.len) : (self.index += 1) {
         const char = self.source[self.index];
@@ -23,6 +29,9 @@ pub fn next(self: *Tokenizer) ?Token {
                 'a'...'z', 'A'...'Z', '_', => {
                     state = .identifier;
                     token.tag = .identifier;
+                },
+                '/' => {
+                    state = .slash;
                 },
                 '{' => {
                     token.tag = .left_brace;
@@ -44,6 +53,26 @@ pub fn next(self: *Tokenizer) ?Token {
                     self.index += 1;
                     break;
                 },
+                ';' => {
+                    token.tag = .semicolon;
+                    self.index += 1;
+                    break;
+                },
+                ',' => {
+                    token.tag = .comma;
+                    self.index += 1;
+                    break;
+                },
+                '+' => {
+                    token.tag = .plus;
+                    self.index += 1;
+                    break;
+                },
+                '=' => {
+                    token.tag = .equals;
+                    self.index += 1;
+                    break;
+                },
                 else => {},
             },
             .identifier => switch (char) {
@@ -51,11 +80,54 @@ pub fn next(self: *Tokenizer) ?Token {
                 else => {
                     const string = self.source[token.start..self.index];
 
-                    _ = string;
-                    //TODO: handle keywords
+                    if (Token.getKeyword(string)) |keyword_tag|
+                    {
+                        token.tag = keyword_tag;
+                    }
 
                     break;
                 },
+            },
+            .slash => switch (char) {
+                '/' => {
+                    state = .single_comment;
+                },
+                '*' => {
+                    state = .multi_comment;
+                },
+                else => {},
+            },
+            .single_comment => switch (char) {
+                '\n' => {
+                    state = .start;
+                    token.start = self.index + 1;  
+                },
+                else => {},
+            },
+            .multi_comment => switch (char) {
+                '*' => {
+                    if (self.source[self.index + 1] == '/')
+                    {
+                        self.index += 1;
+
+                        if (multi_comment_level == 0)
+                        {
+                            state = .start;
+                        }
+                        else 
+                        {
+                            multi_comment_level -= 1;
+                        }
+                    }
+                },
+                '/' => {
+                    if (self.source[self.index + 1] == '*')
+                    {
+                        self.index += 1;
+                        multi_comment_level += 1;
+                    }
+                },
+                else => {},
             },
         }
     }
@@ -78,10 +150,26 @@ pub const Token = struct  {
     pub const Tag = enum {
         end,
         keyword_void,
+        keyword_float,
+        keyword_return,
         identifier,
         left_brace,
         right_brace,
         left_paren,
         right_paren,
+        semicolon,
+        comma,
+        plus,
+        equals,
     };
+
+    const keywords = std.ComptimeStringMap(Tag, .{
+        .{ "void", .keyword_void },
+        .{ "float", .keyword_float },
+        .{ "return", .keyword_return },
+    });
+
+    pub fn getKeyword(string: []const u8) ?Tag {
+        return keywords.get(string);
+    }
 };
--- src/test.glsl
+++ src/test.glsl
@@ -1,4 +1,14 @@
 
+//adds two numbers, a and b
+float add(float a, float b) {
+    return a + b;
+}
+
+/* 
+    entrypoint procedure
+
+    good luck!
+*/
 void main() {
-    
+    float res = add(1, 2);
 }
